b0fd6b765f457c533277fb7234371918c2940d8f
zhangxinnan <zhangxin19870504@163.com>
2024-07-05 08:41:10 +0800

    latin

---
+++ dataset_img.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+# encoding: utf-8
+import os
+import random
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+# import lmdb
+import six
+import sys
+from PIL import Image
+import numpy as np
+
+
+class ImageDataset(Dataset):
+
+    def __init__(self, root, file_path, transform=None, target_transform=None, train_mode=True):
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.root = root
+        self.img_list = []
+        with open(file_path, 'r', encoding='utf-8') as fi:
+            for line in fi:
+                line = line.strip()
+                arr = line.split('\t')
+                if len(arr) < 2:
+                    continue
+                # print(arr)
+                self.img_list.append((arr[0], arr[1]))
+        self.train_mode = train_mode
+        if self.train_mode:
+            random.shuffle(self.img_list)
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'
+        img_path = os.path.join(self.root, self.img_list[index][0])
+        try:
+            img = Image.open(img_path).convert('L')
+        except IOError:
+            print('Corrupted image for %d' % index)
+            return self[index + 1]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        label = self.img_list[index][1]
+        if self.target_transform is not None:
+            label = self.target_transform(label)
+
+        return (img, label)
+
+
+class resizeNormalize(object):
+
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, img):
+        img = img.resize(self.size, self.interpolation)
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        return img
+
+
+class randomSequentialSampler(sampler.Sampler):
+
+    def __init__(self, data_source, batch_size):
+        self.num_samples = len(data_source)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        n_batch = len(self) // self.batch_size
+        tail = len(self) % self.batch_size
+        index = torch.LongTensor(len(self)).fill_(0)
+        for i in range(n_batch):
+            random_start = random.randint(0, len(self) - self.batch_size)
+            batch_index = random_start + torch.range(0, self.batch_size - 1)
+            index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
+        # deal with tail
+        if tail:
+            random_start = random.randint(0, len(self) - self.batch_size)
+            tail_index = random_start + torch.range(0, tail - 1)
+            index[(i + 1) * self.batch_size:] = tail_index
+
+        return iter(index)
+
+    def __len__(self):
+        return self.num_samples
+
+
+class alignCollate(object):
+
+    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
+        self.imgH = imgH
+        self.imgW = imgW
+        self.keep_ratio = keep_ratio
+        self.min_ratio = min_ratio
+
+    def __call__(self, batch):
+        images, labels = zip(*batch)
+
+        imgH = self.imgH
+        imgW = self.imgW
+        if self.keep_ratio:
+            ratios = []
+            for image in images:
+                w, h = image.size
+                ratios.append(w / float(h))
+            ratios.sort()
+            max_ratio = ratios[-1]
+            imgW = int(np.floor(max_ratio * imgH))
+            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
+
+        transform = resizeNormalize((imgW, imgH))
+        images = [transform(image) for image in images]
+        images = torch.cat([t.unsqueeze(0) for t in images], 0)
+
+        return images, labels
+
+
+if __name__ == '__main__':
+    # img_dir = r'C:\gitlab\synthetic_data_spanish\results_generate'
+    # file_path = r'C:\gitlab\synthetic_data_spanish\results_generate\val_list.txt'
+    img_dir = r'C:\github\PaddleOCR\train_data'
+    file_path = r'C:\github\PaddleOCR\train_data\bg3_val_labels_80.txt'
+    imgH, imgW = 32, 100
+    train_dataset = ImageDataset(img_dir, file_path)
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=2,
+        shuffle=True, sampler=None,
+        num_workers=2,
+        collate_fn=alignCollate(imgH=imgH, imgW=imgW, keep_ratio=False))
+    for img, label in train_loader:
+        print(img.shape, img.dtype, img.max(), img.min())
+        print(label)
+        break
